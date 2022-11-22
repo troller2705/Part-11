32b0d983222fedcfc28fe3c6b4b7a59c0c7a2b9d
Cole <cole.groves2705@gmail.com>
2022-11-22 09:25:34 -0500

    Add project files.

---
+++ MyMethods.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Part_11___Methods
+{
+    internal class MyMethods
+    {
+        public static double GetHypotenuse(double a, double b)
+        {
+            double c = Math.Sqrt((a * a) + (b * b));
+            return c;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Part_11___Methods
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double a, b;
+
+            Console.Write("Input value 'a' of the right angle triangle: ");
+            double.TryParse(Console.ReadLine(), out a);
+            Console.Write("Input value 'b' of the right angle triangle: ");
+            double.TryParse(Console.ReadLine(), out b);
+            
+            Console.WriteLine(MyMethods.GetHypotenuse(a, b));
+        }
+    }
+}
